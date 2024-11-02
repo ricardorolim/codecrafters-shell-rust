185b4f382cb5a07da2d918ef8b4076e158be1205
Rickroll <amazingrickroll@gmail.com>
2024-11-02 07:11:21 +0000

    The exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
@@ -11,6 +12,15 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        println!("{}: command not found", input.trim());
+        let res = eval(input.trim());
+
+        println!("{}", res);
+    }
+}
+
+fn eval(input: &str) -> String {
+    match input {
+        input if input.starts_with("exit") => exit(0),
+        _ => format!("{}: command not found", input),
     }
 }
