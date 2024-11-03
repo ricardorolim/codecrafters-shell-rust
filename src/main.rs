9b0ef9b2cc4e267f0087804cb2f279cd75fd32a2
Rickroll <amazingrickroll@gmail.com>
2024-11-03 07:21:26 +0000

    The cd builtin: Relative paths

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use std::env;
-use std::fmt::format;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::process::{exit, Command};
+
+enum Builtin {
+    Exit,
+    Echo,
+    Type,
+    Pwd,
+    Cd,
+    Unknown,
+}
+
+impl From<&str> for Builtin {
+    fn from(cmd: &str) -> Self {
+        match cmd {
+            "exit" => Builtin::Exit,
+            "echo" => Builtin::Echo,
+            "type" => Builtin::Type,
+            "pwd" => Builtin::Pwd,
+            "cd" => Builtin::Cd,
+            _ => Builtin::Unknown,
+        }
+    }
+}
 
 fn main() {
     loop {
@@ -25,25 +46,32 @@ fn eval(input: &str) -> Option<String> {
     let words: Vec<&str> = input.split_whitespace().collect();
     let (cmd, args) = words.split_first().unwrap();
 
-    match *cmd {
-        "exit" => exit(0),
-        "echo" => Some(args.join(" ")),
-        "type" => match args[0] {
-            "exit" | "echo" | "type" | "pwd" => Some(format!("{} is a shell builtin", args[0])),
-            _ => {
-                let filename = args[0];
-                match find(filename) {
-                    Some(fullpath) => Some(format!("{} is {}", filename, fullpath.display())),
-                    None => Some(format!("{}: not found", filename)),
-                }
-            }
-        },
-        "pwd" => Some(env::current_dir().unwrap().to_string_lossy().to_string()),
-        "cd" => match env::set_current_dir(args[0]) {
-            Ok(..) => None,
-            Err(..) => Some(format!("cd: {}: No such file or directory", args[0])),
-        },
+    match Builtin::from(*cmd) {
+        Builtin::Exit => exit(0),
+        Builtin::Echo => Some(args.join(" ")),
+        Builtin::Type => handle_type(args),
+        Builtin::Pwd => Some(env::current_dir().unwrap().to_string_lossy().to_string()),
+        Builtin::Cd => handle_cd(args),
         _ => run_program(cmd, args),
+    }
+}
+
+fn handle_type(args: &[&str]) -> Option<String> {
+    if let Builtin::Unknown = Builtin::from(args[0]) {
+        let filename = args[0];
+        match find(filename) {
+            Some(fullpath) => Some(format!("{} is {}", filename, fullpath.display())),
+            None => Some(format!("{}: not found", filename)),
+        }
+    } else {
+        Some(format!("{} is a shell builtin", args[0]))
+    }
+}
+
+fn handle_cd(args: &[&str]) -> Option<String> {
+    match env::set_current_dir(args[0]) {
+        Ok(..) => None,
+        Err(..) => Some(format!("cd: {}: No such file or directory", args[0])),
     }
 }
 
